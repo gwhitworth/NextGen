80e38fed3b6abf4cd99cc320a8284c6bc1386474
Gerry Whitworth <gerrywhitworth@sierrasystems.com>
2019-04-12 11:51:48 -0700

    check in now before trying to line up boxes

---
+++ NextGen/SQL/Pivot Example.sql
@@ -0,0 +1,31 @@
+DROP TABLE #temp
+GO
+CREATE TABLE #temp
+(
+    ProType varchar(15),
+    yearVal varchar(4),
+    ValType varchar(10),
+	Value money
+)
+insert into #temp values('Land', '2019', 'Assessed', 268000)
+insert into #temp values('Land', '2020', 'Actual', 268514)
+insert into #temp values('Improvements', '2019', 'Assessed', 183000)
+insert into #temp values('Improvements', '2020', 'Actual', 183474)
+
+SELECT ProType, [2019 Assessed],[2020 Actual]
+FROM
+(
+	SELECT
+		ProType,
+		Concat(yearVal, ' ' , ValType) AS [Section] ,
+		Value as [data]
+	FROM #temp
+	
+)x
+
+PIVOT
+(
+	max([data])
+	FOR [Section] IN([2019 Assessed],[2020 Actual])
+) AS p
+ORDER BY ProType DESC
